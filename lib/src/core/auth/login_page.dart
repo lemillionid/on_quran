1b78f4b128d5e1e4701c02ee6fe27d9f9ec42e8a
lemillionid <dedeandriansyah0@gmail.com>
2023-06-07 08:14:17 +0700

    Penambahan asset dan pembaharuan halaman login

--- lib/src/core/auth/login_page.dart
+++ lib/src/core/auth/login_page.dart
@@ -6,8 +6,63 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/login_illus.png",
+                height: 300,
+                width: 300,
+              ),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Assalammu'alaikum",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Roboto",
+                            ),
+                          ),
+                          Text(
+                            "Login to continue using the app",
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Roboto",
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(
+                        Icons.lock_open_rounded,
+                        size: 25,
+                        color: Colors.blue.shade800,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              TextField()
+            ],
+          ),
+        ),
       ),
     );
   }
