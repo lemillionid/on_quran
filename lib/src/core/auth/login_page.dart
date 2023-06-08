b2ee7180be2c01c0c9cad103fe86e6be08a188b8
lemillionid <dedeandriansyah0@gmail.com>
2023-06-08 11:56:20 +0700

    Login Page UI UX onQuran

--- lib/src/core/auth/login_page.dart
+++ lib/src/core/auth/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:on_quran_uiux/routes/routes_screen.dart';
+import 'package:on_quran_uiux/src/core/auth/forgot_pass_page.dart';
+import 'package:on_quran_uiux/src/core/auth/signup_page.dart';
 import 'package:on_quran_uiux/src/screen/home/home_screen.dart';
 
 class LoginPage extends StatefulWidget {
@@ -35,8 +36,8 @@ class _LoginPageState extends State<LoginPage> {
             children: [
               Image.asset(
                 "assets/images/login_illus.png",
-                height: 300,
-                width: 300,
+                height: 250,
+                width: 250,
               ),
               const SizedBox(height: 20),
               Column(
@@ -54,42 +55,72 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
               const SizedBox(height: 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Username",
-                  prefixIcon: Icon(Icons.person),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: const BorderRadius.all(Radius.circular(17))),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Username",
+                    border: InputBorder.none,
+                  ),
                 ),
               ),
               const SizedBox(height: 20),
-              TextFormField(
-                obscureText: !_passwordVisible,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: const Icon(Icons.lock_open_rounded),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
-                    icon: Icon(_passwordVisible
-                        ? Icons.visibility_rounded
-                        : Icons.visibility_off_rounded),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: const BorderRadius.all(Radius.circular(17))),
+                child: TextFormField(
+                  obscureText: !_passwordVisible,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: InputBorder.none,
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                      icon: Icon(_passwordVisible
+                          ? Icons.visibility_rounded
+                          : Icons.visibility_off_rounded),
+                    ),
                   ),
                 ),
               ),
               const SizedBox(height: 10),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Checkbox(
-                    value: _rememberMe,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _rememberMe = value!;
-                      });
-                    },
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: _rememberMe,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _rememberMe = value!;
+                          });
+                        },
+                      ),
+                      const Text('Remember me'),
+                    ],
                   ),
-                  const Text('Remember me'),
+                  TextButton(
+                    onPressed: () {
+                      MaterialPageRoute(
+                          builder: (context) => const ForgotPassPage());
+                    },
+                    child: const Text("Forgot Password?"),
+                  )
                 ],
               ),
               const SizedBox(height: 10),
@@ -99,8 +130,8 @@ class _LoginPageState extends State<LoginPage> {
                 decoration: BoxDecoration(
                     gradient: LinearGradient(
                       colors: _isPressed
-                          ? [Colors.teal, Colors.blueAccent]
-                          : [Colors.green, Colors.greenAccent],
+                          ? [Colors.blueAccent, Colors.lightGreen]
+                          : [Colors.green, Colors.blue],
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
                     ),
@@ -116,6 +147,8 @@ class _LoginPageState extends State<LoginPage> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.transparent,
                     elevation: 0,
+                    splashFactory: NoSplash.splashFactory,
+                    shadowColor: Colors.transparent,
                   ),
                   child: const Text(
                     "Sign In",
@@ -126,6 +159,96 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 2,
+                      height: 2,
+                      endIndent: 20,
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                  const Text("or login with"),
+                  Expanded(
+                    child: Divider(
+                      thickness: 2,
+                      height: 2,
+                      indent: 20,
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 2,
+                        )),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Image.asset("assets/icons/google.png")),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 2,
+                        )),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Image.asset("assets/icons/facebook.png")),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 2,
+                        )),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Image.asset("assets/icons/apple.png")),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("don't have an account?"),
+                  TextButton(
+                    onPressed: () {
+                      MaterialPageRoute(
+                          builder: (context) => const SignUpPage());
+                    },
+                    child: const Text("Sign Up"),
+                  ),
+                ],
+              )
             ],
           ),
         ),
