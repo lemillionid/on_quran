eed6f177c49df127e3151889cf8a2764a9a2c22e
lemillionid <dedeandriansyah0@gmail.com>
2023-06-09 09:47:52 +0700

    Sign up page

--- lib/src/core/auth/signup_page.dart
+++ lib/src/core/auth/signup_page.dart
@@ -11,6 +11,16 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "onQuran",
+          style: TextStyle(
+            fontFamily: "ArabicCaligraphy",
+            fontSize: 35,
+            color: Colors.white,
+          ),
+        ),
+      ),
       body: Container(
         padding: const EdgeInsets.all(30),
         color: Colors.white,
