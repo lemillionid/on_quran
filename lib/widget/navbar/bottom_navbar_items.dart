a4d61fc271b6b6f602f942ee5441b57e8b0fd420
Dede Andriansyah <dedeandriansyah0@gmail.com>
2023-07-04 10:38:25 +0700

    tes

---
+++ lib/widget/navbar/bottom_navbar_items.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final List<Widget> navigationItems = [
+  Image.asset('assets/icons/home.png', height: 30),
+  Image.asset('assets/icons/qibla.png', height: 30),
+  Image.asset('assets/icons/quran-2.png', height: 30),
+  Image.asset('assets/icons/live.png', height: 30),
+  Image.asset('assets/icons/setting.png', height: 30),
+];
