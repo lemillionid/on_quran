ba086601ff80371f64f1fb221355e32bba917684
Dede Andriansyah <dedeandriansyah0@gmail.com>
2023-08-07 10:04:04 +0700

    Menambahkan Font Versi Developer

--- lib/src/features/quran/quran_screen.dart
+++ lib/src/features/quran/quran_screen.dart
@@ -25,51 +25,11 @@ class _QuranScreenState extends ConsumerState<QuranScreen> {
                     'Quran Test',
                     textAlign: TextAlign.start,
                   ),
-                  // const SizedBox(height: 10),
-                  // Container(
-                  //   decoration: BoxDecoration(
-                  //     border: Border.all(
-                  //       color: Colors.grey,
-                  //       width: 1,
-                  //     ),
-                  //   ),
-                  //   width: 250,
-                  //   padding: const EdgeInsets.all(10),
-                  //   child: const Directionality(
-                  //     textDirection: TextDirection.rtl,
-                  //     child: Column(
-                  //       crossAxisAlignment: CrossAxisAlignment.stretch,
-                  //       children: [
-                  //         Text(
-                  //           ' إِنَّٱلَّذِينَ كَفَرُواْسَوَآءٌعَلَيۡهِمۡءَأَنذَرۡتَهُمۡ أَمۡ لَمۡ تُنذِرۡهُمۡ ',
-                  //           style: TextStyle(
-                  //             fontFamily: 'QuranFont',
-                  //           ),
-                  //           textAlign: TextAlign.justify,
-                  //         ),
-                  //         Text(
-                  //           'لَايُؤۡمِنُونَ ٦ خَتَمَ ٱللَّهُ عَلَىٰ قُلُوبِهِمۡ وَعَلَىٰ سَمۡعِهِمۡۖ وَعَلَىٰٓ',
-                  //           style: TextStyle(
-                  //             fontFamily: 'QuranFont',
-                  //           ),
-                  //           textAlign: TextAlign.justify,
-                  //         ),
-                  //         Text(
-                  //           'أَبۡصَٰرِهِمۡ  غِشَٰوَةٞۖ وَلَهُمۡ عَذَابٌ عَظِيمٞ ٧ وَمِنَ ٱلنَّاسِ',
-                  //           style: TextStyle(
-                  //             fontFamily: 'QuranFont',
-                  //           ),
-                  //           textAlign: TextAlign.justify,
-                  //         ),
-                  //       ],
-                  //     ),
-                  //   ),
-                  // ),
                   const SizedBox(height: 10),
                   Container(
                     decoration: BoxDecoration(
                       border: Border.all(
-                        color: Colors.grey,
+                        color: Colors.white,
                         width: 1,
                       ),
                     ),
@@ -82,35 +42,138 @@ class _QuranScreenState extends ConsumerState<QuranScreen> {
                         text: TextSpan(
                           style: DefaultTextStyle.of(context).style,
                           children: const <TextSpan>[
+                            //Baris1
                             TextSpan(
                               text:
                                   'إِنَّ ٱلَّذِينَ كَفَرُواْسَوَآءٌعَلَيۡهِمۡءَأَنذَرۡتَهُمۡ أَمۡ لَمۡ تُنـذِرۡهُمۡ ',
                               style: TextStyle(
-                                fontFamily: 'QuranFont',
+                                fontFamily: 'QuranFont2',
                                 fontSize: 20,
                               ),
                             ),
+                            //Baris2
                             TextSpan(
                               text:
                                   'لَايُؤۡمِنُونَ ٦ خَتَمَ ٱللَّهُ عَلَىٰ قُلُوبِهِمۡ وَعَلَىٰ سَمۡعِهِمۡۖ وَعَلَىٰٓ',
                               style: TextStyle(
-                                fontFamily: 'QuranFont',
+                                fontFamily: 'QuranFont2',
                                 fontSize: 20,
                               ),
                             ),
+                            //Baris3
                             TextSpan(
                               text:
                                   ' أَبۡصَٰرِهِمۡ  غِشَٰوَةٞۖ وَلَهُمۡ عَذَابٌ عَظِيمٞ  ٧ وَمِنَ ٱلنَّاسِ',
                               style: TextStyle(
-                                fontFamily: 'QuranFont',
+                                fontFamily: 'QuranFont2',
                                 fontSize: 20,
                               ),
                             ),
+                            //Baris4
                             TextSpan(
                               text:
                                   ' مَن يَقُولُ ءَامَنَّا بِٱللَّهِ وَبِٱلۡيَوۡمِ ٱلۡأٓخِرِ وَمَاهُم بِمُؤۡمِنِينَ ٨',
                               style: TextStyle(
-                                fontFamily: 'QuranFont',
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris5
+                            TextSpan(
+                              text:
+                                  ' يُخَٰدِ عُونَ ٱللَّهَ وَٱلَّذِينَ ءَامَنُواْ وَمَايَخۡدَعُوـنَ إِلَّآ أَنفُسَهُمۡ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris6
+                            TextSpan(
+                              text:
+                                  ' وَمَا يَشۡعُرُونَ ٩ فِي قُلُوبِهِم مَّرَضٞ فَزَادَهُـمُ ٱللَّهُ مَرَضٗـاۖ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris7
+                            TextSpan(
+                              text:
+                                  ' وَلَهُمۡ عَذَابٌ أَلِيمُۢ بِمَا كَـانُواْ يَـكۡذِبُوـنَ ١٠ وَإِذَاقِيلَ لَهُمۡ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris8
+                            TextSpan(
+                              text:
+                                  ' لَاتُفۡسِدُواْ فِي ٱلۡأَرۡضِ قَالُوٓاْإِنَّمَانَحۡنُ مُصۡلِحُونَ ١١ أَلَآإِنَّهُمۡ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris9
+                            TextSpan(
+                              text:
+                                  ' هُـمُ ٱلۡمُفۡسِدُونَ وَلَٰكِن لَّايَشۡعُرُوـنَ ١٢ وَإِذَاقِيلَ لَهُمۡ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris10
+                            TextSpan(
+                              text:
+                                  ' ءَامِنُواْكَمَآءَامَنَٱلنَّاسُ قَالُوٓاْ أَنُؤۡمِنُ كَمَآءَامَنَٱلسُّفَهَآءُۗ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris11
+                            TextSpan(
+                              text:
+                                  ' أَلَآإِنَّهُمۡ هُمُ ٱلسُّفَهَآءُوَلَٰكِن لَّايَعۡلَمُونَ ١٣ وَإِذَالَقُواْ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris12
+                            TextSpan(
+                              text:
+                                  ' ٱلَّذِينَءَامَنُواْقَالُوٓاْءَامَنَّـا وَإِذَا خَلَوۡاْ إِلَىٰ شَيَٰطِينِهِمۡ قَالُوٓاْإِنَّا',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris13
+                            TextSpan(
+                              text:
+                                  ' مَعَكُمۡ إِنَّمَانَحۡنُمُسۡتَهۡزِءُونَ ١٤ٱللَّهُ يَسۡتَهۡزِئُ بِهِمۡ وَيَمُدُّهُمۡ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris14
+                            TextSpan(
+                              text:
+                                  ' فِي طُغۡيَٰنِهِمۡ يَعۡمَهُونَ ١٥ أُوْلَٰٓئِكَ ٱلَّذِينَ ٱشۡتَرَوُاْ ٱلضَّلَٰلَةَ ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
+                                fontSize: 20,
+                              ),
+                            ),
+                            //Baris15
+                            TextSpan(
+                              text:
+                                  ' بِٱلۡهُدَىٰ فَمَا رَبِحَت تِّجَٰرَ تُهُمۡ وَمَا كَـانُواْ مُهۡتَدِيـــنَ ١٦ ',
+                              style: TextStyle(
+                                fontFamily: 'QuranFont2',
                                 fontSize: 20,
                               ),
                             ),
