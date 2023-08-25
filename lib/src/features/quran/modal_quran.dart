87c12186b8f626b3ef039428caac444e059f92da
Dede Andriansyah <dedeandriansyah0@gmail.com>
2023-08-25 16:19:45 +0700

    Finalisasi ModalBottomSheet Quran

--- lib/src/features/quran/modal_quran.dart
+++ lib/src/features/quran/modal_quran.dart
@@ -9,25 +9,33 @@ class ModalQuran extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(10),
       height: 700,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.only(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
           topLeft: Radius.circular(20),
           topRight: Radius.circular(20),
         ),
-        color: Colors.blueAccent.shade700,
+        // gradient: LinearGradient(
+        //   colors: [
+        //     Colors.blueAccent.shade700,
+        //     Colors.purple,
+        //   ],
+        //   begin: Alignment.topCenter,
+        //   end: Alignment.bottomCenter,
+        // ),
+        color: Colors.white,
       ),
       child: Column(
         children: [
           const Icon(
             Icons.maximize_rounded,
-            color: Colors.grey,
+            color: Colors.black,
             size: 30,
           ),
-          const Text(
+          Text(
             'AL QUR\'AN',
             style: TextStyle(
               fontWeight: FontWeight.bold,
-              color: Colors.white,
+              color: Colors.deepPurple.shade900,
             ),
           ),
           const SizedBox(height: 10),
@@ -53,7 +61,7 @@ class ModalQuran extends StatelessWidget {
                         const Text(
                           'Madinah',
                           style: TextStyle(
-                            color: Colors.white,
+                            color: Colors.black,
                           ),
                         ),
                       ],
@@ -73,7 +81,7 @@ class ModalQuran extends StatelessWidget {
                         const Text(
                           'Indonesia',
                           style: TextStyle(
-                            color: Colors.white,
+                            color: Colors.black,
                           ),
                         ),
                       ],
@@ -93,7 +101,7 @@ class ModalQuran extends StatelessWidget {
                         const Text(
                           'Per Kata ',
                           style: TextStyle(
-                            color: Colors.white,
+                            color: Colors.black,
                           ),
                         ),
                       ],
@@ -113,7 +121,7 @@ class ModalQuran extends StatelessWidget {
                         const Text(
                           'Per Ayat ',
                           style: TextStyle(
-                            color: Colors.white,
+                            color: Colors.black,
                           ),
                         ),
                       ],
@@ -141,13 +149,13 @@ class ModalQuran extends StatelessWidget {
                             Text(
                               'Madinah',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                             Text(
                               'Tajwid',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                           ],
@@ -171,13 +179,13 @@ class ModalQuran extends StatelessWidget {
                             Text(
                               'Indonesia',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                             Text(
                               'Tajwid',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                           ],
@@ -201,13 +209,13 @@ class ModalQuran extends StatelessWidget {
                             Text(
                               'Per Kata',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                             Text(
                               'Tajwid',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                           ],
@@ -231,13 +239,13 @@ class ModalQuran extends StatelessWidget {
                             Text(
                               'Donasi',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                             Text(
                               'Mushaf',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.black,
                               ),
                             ),
                           ],
@@ -246,6 +254,203 @@ class ModalQuran extends StatelessWidget {
                     ),
                   ],
                 ),
+                const SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Stack(
+                        alignment: Alignment.centerLeft,
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 150,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent.shade700,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(17)),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  spreadRadius: 1.0,
+                                  offset: Offset(-1, -1),
+                                ),
+                              ],
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/aqwam-jembatan-ilmu-jTvT1OiBVNk-unsplash.jpg'),
+                                fit: BoxFit.cover,
+                                opacity: 0.2,
+                              ),
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Tilawahku',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  'Terdapat 4 Surat',
+                                  style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 15,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: Colors.lightGreen,
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 20),
+                      Stack(
+                        alignment: Alignment.centerLeft,
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 150,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent.shade700,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(17)),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  spreadRadius: 1.0,
+                                  offset: Offset(-1, -1),
+                                ),
+                              ],
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/aqwam-jembatan-ilmu-jTvT1OiBVNk-unsplash.jpg'),
+                                fit: BoxFit.cover,
+                                opacity: 0.2,
+                              ),
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Ayat Pilihan',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '5 Kategori',
+                                  style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 15,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: Colors.orange,
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 20),
+                      Stack(
+                        alignment: Alignment.centerLeft,
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 150,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent.shade700,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(17)),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  spreadRadius: 1.0,
+                                  offset: Offset(-1, -1),
+                                ),
+                              ],
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/aqwam-jembatan-ilmu-jTvT1OiBVNk-unsplash.jpg'),
+                                fit: BoxFit.cover,
+                                opacity: 0.2,
+                              ),
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Khataman Al-Quran',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  'Mulai khataman',
+                                  style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 15,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.red.shade700,
+                              borderRadius: const BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 20),
+                    ],
+                  ),
+                )
               ],
             ),
           )
